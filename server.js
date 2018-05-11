0bd2710b35a0e9e03636e7cad5345235e1dd5e54
Nat Schulz <natalie.schulz@me.com>
2018-05-10 18:04:46 -0700

    set ENV to back to develpoment and added example.env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
-const ENV = process.env.ENV || "production";
+const ENV = process.env.ENV || "development";
 const app = express();
 const dev = app.get('env') !== 'production';
 
