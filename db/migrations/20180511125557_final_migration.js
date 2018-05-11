cc938a25efc070f0b5a5f202373dcd037a4adc03
Sam Schantz <samvschantz@gmail.com>
2018-05-11 13:38:21 -0700

    final migration debugged

--- db/migrations/20180511125557_final_migration.js
+++ db/migrations/20180511125557_final_migration.js
@@ -9,11 +9,11 @@ exports.up = function(knex, Promise) {
     }),
     knex.schema.table('volunteers', function (t){
       t.varchar('pic_url', 255)
-      table.renameColumn('vol_name', 'name'),
-      table.renameColumn('vol_email', 'email'),
-      table.dropColumn('vol_hours'),
-      table.renameColumn('vol_password', 'password'),
-    })
+      t.renameColumn('vol_name', 'name'),
+      t.renameColumn('vol_email', 'email'),
+      t.dropColumn('vol_hours'),
+      t.renameColumn('vol_password', 'password')
+    }),
     knex.schema.table('events', function (t){
       t.varchar('event_type', 100)
       //nothing else changes
@@ -35,7 +35,7 @@ exports.down = function(knex, Promise) {
       t.varchar('vol_email', 255),
       t.varchar('vol_hours', 255),
       t.varchar('vol_password', 255)
-    })
+    }),
     knex.schema.table('events', function (t){
       t.dropColumn('event_type')
     })
